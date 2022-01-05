1903d30e2507e97263170aff483123fff00e0c79
Soebb <87156166+Soebb@users.noreply.github.com>
2022-01-05 18:11:50 +0330

    Update autho_correct.py

--- autho_correct.py
+++ autho_correct.py
@@ -1,4 +1,35 @@
 import nltk
+from pyrogram import Client, filters
+
+
+# config vars
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+API_ID = os.environ.get("API_ID")
+API_HASH = os.environ.get("API_HASH")
+
+Bot = Client(
+    "Bot",
+    bot_token = BOT_TOKEN,
+    api_id = API_ID,
+    api_hash = API_HASH
+)
+
+START_TXT = """
+Hi {}
+I am Text Corrector Bot.
+
+> `I can correct Persian text for you.`
+
+Send me a text message to get started.
+"""
+
+@Bot.on_message(filters.command(["start"]))
+async def start(bot, update):
+    text = START_TXT.format(update.from_user.mention)
+    await update.reply_text(
+        text=text,
+        disable_web_page_preview=True
+    )
 
 
 def read_file(path):
@@ -10,20 +41,23 @@ def read_file(path):
 
 words=read_file('big.txt')
 
+@Bot.on_message(filters.private & filters.text)
+async def main(bot, m):
+    text = m.text
+    new_text = ""
+    for sntnce in text.rsplitlines():
+        sntnce_splited = sntnce.rsplit()
+        for x in sntnce_splited:
+            if x not in words:
+                for word in words:
+                    corrected_word = nltk.edit_distance(x,word)
+                sntnce_splited[sntnce_splited.index(x)] = corrected_word
+        new_text += sntnce_splited.join(" ") + "\n"
 
-text = input("Paste your text: ")
-new_text = ""
-
-for sntnce in text.rsplitlines():
-    sntnce_splited = sntnce.rsplit()
-    for x in sntnce_splited:
-        if x not in words:
-            for word in words:
-                corrected_word = nltk.edit_distance(x,word)
-            sntnce_splited[sntnce_splited.index(x)] = corrected_word
-    new_text += sntnce_splited.join(" ") + "\n"
+    await m.reply(new_text)
 
 
-print(new_text)
+
+Bot.run()
 
 
