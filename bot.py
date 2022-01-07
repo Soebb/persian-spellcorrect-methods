aebae8be24121b4e9dfcfc3cbf260d5dc0c5db9d
Soebb <87156166+Soebb@users.noreply.github.com>
2022-01-08 01:59:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ async def main(bot, m):
         sntnce_splited = sntnce.rsplit()
         for x in sntnce_splited:
             if x not in words:
-                corrected_word = correction(word)
+                corrected_word = correction(x)
                 sntnce_splited[sntnce_splited.index(x)] = str(corrected_word)
         new_text += " ".join(sntnce_splited)
         new_text += "\n"
